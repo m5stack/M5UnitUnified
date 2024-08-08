072bdaf6a60c4549cccae666c08e25ce33e4aa40
GOB <goblin52@gmail.com>
2024-08-08 15:34:13 +0900

    Separate lib/M5utility

--- lib/M5Utility/test/expected/noexcept.cpp
+++
@@ -1,6 +0,0 @@
-#include <gtest/gtest.h>
-#include <m5_utility/stl/expected.hpp>
-
-TEST(Expected, Noexcept) {
-    //TODO
-}
--- lib/M5Utility/test/optional/hash.cpp
+++
@@ -1,4 +0,0 @@
-#include <gtest/gtest.h>
-#include <m5_utility/stl/optional.hpp>
-
-TEST(Optional, Hashing) {}
