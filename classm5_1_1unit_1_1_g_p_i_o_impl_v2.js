f85ba10bc3e524df822acc046d26472186d39d94
GOB52 <GOB52@users.noreply.github.com>
2026-06-08 17:32:10 +0000

    Deploying to gh-pages from @ m5stack/M5UnitUnified@23e84e769d23d4a054670f51b643465a82a08c6d 🚀

--- classm5_1_1unit_1_1_g_p_i_o_impl_v2.js
+++
@@ -1,4 +0,0 @@
-var classm5_1_1unit_1_1_g_p_i_o_impl_v2 =
-[
-    [ "callback_struct_t", "structm5_1_1unit_1_1_g_p_i_o_impl_v2_1_1callback__struct__t.html", null ]
-];
--- search/classes_7.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['spiimpl_0',['SPIImpl',['../classm5_1_1unit_1_1_adapter_s_p_i_1_1_s_p_i_impl.html',1,'m5::unit::AdapterSPI']]]
-];
